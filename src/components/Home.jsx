63454f9a1500ecbc1dad467ee4815a91a41c500b
vincent292 <ariasvincent292@gmail.com>
2025-02-17 17:29:41 -0400

    3 commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Carousel from "./Carousel";
+import "./style/home.css";
 
 
 const Home = () => {
   return (
     <div>
+
  <h1>¡Feliz San Valentín! ❤️ Amor de mi vida ❤️</h1>
         <Carousel />
-     
+     <div className="container-home">  
+      
       <h2>este es un proyecto que estaba haciendo hace un tiempo y me parecio muy bontio</h2>
       <h3>no es mucho pero es lo que pude hacer por todo el amor que siento por ti</h3>
       <h3>Ya que hacer una manualidad es un poco dificil...:(</h3>
       <h3>estoy pensando agregar mas cosas y hacerlo aglo muy bonito </h3>
 
+     </div>
 
 
 
